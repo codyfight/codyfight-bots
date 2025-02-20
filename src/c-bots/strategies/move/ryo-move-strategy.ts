96310b1f59c5e22cd685f2b50365b30089dbafa2
irish-mike <michaelgr@live.ie>
2025-02-20 16:25:12 +0100

    Create ryo-move-strategy.ts

--- src/c-bots/strategies/move/ryo-move-strategy.ts
+++ src/c-bots/strategies/move/ryo-move-strategy.ts
@@ -5,12 +5,10 @@ import { SpecialAgentType } from '../../../game/agents/game-agent.type.js'
 class RyoMoveStrategy extends MoveStrategy {
   protected setTargets(): void {
     const ryoAgents = this.specialAgents.get(SpecialAgentType.MrRyo);
-    
     if (ryoAgents && ryoAgents.length > 0) {
       this.targets.push(ryoAgents[0].getPosition());
     }
   }
-
 }
 
 export default RyoMoveStrategy
