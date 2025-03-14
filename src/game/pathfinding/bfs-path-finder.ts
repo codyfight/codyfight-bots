990b0e590062e6be21a762630d0b7b9aa99ee9be
irish-mike <michaelgr@live.ie>
2025-03-14 11:54:50 +0100

    Comment to bfs pathfinder

--- src/game/pathfinding/bfs-path-finder.ts
+++ src/game/pathfinding/bfs-path-finder.ts
@@ -5,6 +5,13 @@ import { IAgentState } from '../agents/game-agent.type.js'
 
 type GoalTest = (state: IAgentState, target: Position) => boolean;
 
+// This algorithm is a breadth-first search, it does not use a heuristic to determine the best path, it assumes that every step is equal to the next.
+// This means that the agent sees casting a movement skill as one step, and moving to the target position as one step.
+// So it is finding the shortest distance but not the optimal path,
+// Ideally if we wanted to improve this so the agent would find the route to the target in the shortest amount of turns,
+// we would use uniform cost or A* search, this includes a path cost function that would determine the cost of each step.
+// E.G. Casting a skill to move = 0 cost, Moving to a position = 1 cost, the cost is the use of a turn.
+
 export default class BFSPathfinder {
   private readonly visited = new Set<string>();
   private readonly frontier: GameNode[] = [];
